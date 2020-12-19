169d163375a057d304b72c05bfbca057c8d46efd
jeonghoheo <heojeongho1992@gmail.com>
2020-12-19 13:38:55 +0900

    add - Arguments

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,10 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurants.entity';
 
-@Resolver()
+@Resolver(() => Restaurant)
 export class RestaurantResolver {
-  @Query(() => Restaurant) // Graphql을 위한것 따라서 Type양식은 Graphql의것을 따라간다
-  myRestaurant(): boolean {
-    return true;
+  @Query(() => [Restaurant]) // Graphql을 위한것 따라서 Type양식은 Graphql의것을 따라간다
+  Restaurant(@Args('valueOnly') valueOnly: boolean): Restaurant[] {
+    return [];
   }
 }
