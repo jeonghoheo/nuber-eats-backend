88b05edc200979479baa54b27d8ac193942f3b29
jeonghoheo <heojeongho1992@gmail.com>
2020-12-19 13:27:01 +0900

    add - ObjectType

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,10 @@
 import { Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurants.entity';
 
 @Resolver()
 export class RestaurantResolver {
-  @Query(() => Boolean) // Graphql을 위한것 따라서 Type양식은 Graphql의것을 따라간다
-  isPizzaGood(): boolean {
+  @Query(() => Restaurant) // Graphql을 위한것 따라서 Type양식은 Graphql의것을 따라간다
+  myRestaurant(): boolean {
     return true;
   }
 }
