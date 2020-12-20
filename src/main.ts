5c7319d8865e848285d9644b0ec039b0871a31a6
jeonghoheo <heojeongho1992@gmail.com>
2020-12-20 13:47:03 +0900

    add - Validating ArgsTypes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // DTO 유효성 검사를 위한 파이프 추가
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
--- src/restaurants/dtos/create-restaurants.dto.ts
+++ src/restaurants/dtos/create-restaurants.dto.ts
@@ -1,13 +1,22 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantsDto {
   @Field(() => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
+
   @Field(() => Boolean)
+  @IsBoolean()
   isVegan: boolean;
+
   @Field(() => String)
+  @IsString()
   adress: string;
+
   @Field(() => String)
+  @IsString()
   ownersName: string;
 }
