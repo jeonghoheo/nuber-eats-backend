c2a327665e6bc21e1edad58755f778c955f3d57a
jeonghoheo <heojeongho1992@gmail.com>
2020-12-22 19:18:24 +0900

    add - Configuring ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,14 +9,15 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '0000',
-      database: 'nuber-eats',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DNAME,
       synchronize: true,
     }),
     GraphQLModule.forRoot({
