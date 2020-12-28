fb323e1bfc0f1df85510c47d1e7107ee9dfefd27
jeonghoheo <heojeongho1992@gmail.com>
2020-12-28 21:56:48 +0900

    add - Create Account Mutation part Three

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -15,7 +15,21 @@ export class UsersResolver {
   }
 
   @Mutation(() => CreateAccountOutput)
-  createAccount(@Args('input') createAccountInput: CreateAccountInput) {
-    return true;
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const error = await this.usersService.createAccount(createAccountInput);
+      if (error) {
+        return {
+          ok: false,
+          error,
+        };
+      } else {
+        return { ok: true };
+      }
+    } catch (error) {
+      return { ok: false, error };
+    }
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,19 +9,20 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
-  async createAccount({ email, password, role }: CreateAccountInput) {
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
     try {
-      const exists = await this.users.findOne({ email });
-      if (exists) {
-        // make Error
-        return;
+      const error = await this.users.findOne({ email });
+      if (error) {
+        return 'There is a user with that email already';
       } else {
         await this.users.save(this.users.create({ email, password, role }));
-        return true;
       }
     } catch (error) {
-      //make Error
-      return;
+      return "Couldn't create account";
     }
   }
 }
