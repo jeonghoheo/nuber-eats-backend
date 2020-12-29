15eb142f37057f3e8807496b0d90e661eebf0ee3
jeonghoheo <heojeongho1992@gmail.com>
2020-12-29 17:56:41 +0900

    add - An Alternative Error

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -13,16 +13,17 @@ export class UsersService {
     email,
     password,
     role,
-  }: CreateAccountInput): Promise<string | undefined> {
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     try {
       const error = await this.users.findOne({ email });
       if (error) {
-        return 'There is a user with that email already';
+        return { ok: false, error: 'There is a user with that email already' };
       } else {
         await this.users.save(this.users.create({ email, password, role }));
+        return { ok: true };
       }
     } catch (error) {
-      return "Couldn't create account";
+      return { ok: false, error: "Couldn't create account" };
     }
   }
 }
