755490e85bc5df3ae06226ea1488fbbcf23d6459
jeonghoheo <heojeongho1992@gmail.com>
2021-01-01 12:32:23 +0900

    add - JWT Module part Two

---
+++ src/jwt/jwt.interfaces.ts
@@ -0,0 +1,4 @@
+export interface JwtModuleOptions {
+  global?: boolean;
+  privateKey: string;
+}
--- src/jwt/jwt.module.ts
+++ src/jwt/jwt.module.ts
@@ -1,16 +1,24 @@
 import { DynamicModule, Module } from '@nestjs/common';
+import { CONFIG_OPTIONS } from './jwt.constants';
+import { JwtModuleOptions } from './jwt.interfaces';
 import { JwtService } from './jwt.service';
 
 @Module({
   providers: [JwtService],
 })
 export class JwtModule {
-  static forRoot(option?: Partial<{ global: boolean }>): DynamicModule {
+  static forRoot(options: JwtModuleOptions): DynamicModule {
     return {
       module: JwtModule,
-      global: option?.global,
+      global: options?.global,
       exports: [JwtService],
-      providers: [JwtService],
+      providers: [
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: options,
+        },
+        JwtService,
+      ],
     };
   }
 }
