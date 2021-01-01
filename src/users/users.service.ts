ae1f4f547cc08bebe553b10136dff833a3ec1646
jeonghoheo <heojeongho1992@gmail.com>
2021-01-01 12:44:15 +0900

    add - JWT Module part Three

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import * as jwt from 'jsonwebtoken';
 import { CreateAccountInput } from './dtos/create-account-dto';
 import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
-import { ConfigService } from '@nestjs/config';
 import { JwtService } from 'src/jwt/jwt.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
-    private readonly config: ConfigService,
     private readonly jwtService: JwtService,
   ) {}
   async createAccount({
@@ -44,7 +41,7 @@ export class UsersService {
         };
       }
       const passwordCorrect = await user.checkPassword(password);
-      const token = jwt.sign({ id: user.id }, this.config.get('SECRET_KEY'));
+      const token = this.jwtService.sign(user.id);
       return {
         ok: passwordCorrect,
         error: passwordCorrect ? null : 'Wrong Password',
