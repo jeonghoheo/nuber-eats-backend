3ae68c70b782055a9cb590a4c4b8a1c71ef387bb
jeonghoheo <heojeongho1992@gmail.com>
2021-01-02 14:59:21 +0900

    add - GraphQl Context

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import {
   CreateAccountInput,
   CreateAccountOutput,
@@ -46,5 +46,11 @@ export class UsersResolver {
   }
 
   @Query(() => User)
-  me() {}
+  me(@Context() context) {
+    if (!context.user) {
+      return;
+    } else {
+      return context.user;
+    }
+  }
 }
