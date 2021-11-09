1a4fc664326d6594c693a0407ecac7d869f269fa
허정호 <heojeongho1992@gmail.com>
2021-11-09 21:02:07 +0900

    add - ObjectType for database and graphql
    - 데이터베이스에서 다뤄줄 타입 만들어주기

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 @Module({
-  imports: [GraphQLModule.forRoot()],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    RestaurantsModule,
+  ],
   controllers: [],
   providers: [],
 })
