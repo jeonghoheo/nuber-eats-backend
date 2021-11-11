f7a0af82927578e0344e58c4d0bf96664857c7ce
허정호 <heojeongho1992@gmail.com>
2021-11-11 19:56:53 +0900

    add - Arguments type

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,17 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  ArgsType,
+  Field,
+  ObjectType,
+  Query,
+  Resolver,
+} from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver()
 export class RestaurantsResolver {
-  @Query((returns) => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query((returns) => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
   }
 }
