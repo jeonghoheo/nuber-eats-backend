6437eaf97cbce1d9ea21943d37fceb59130b0409
허정호 <heojeongho1992@gmail.com>
2021-11-17 20:52:16 +0900

    add - joi를 통해 schema 유효성 검사하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+
+const ENV_VAR = {
+  dev: 'dev',
+  prod: 'prod',
+  test: 'test',
+};
 
 const ENV_PATH = {
   dev: '.env.dev',
@@ -13,13 +20,23 @@ const ENV_PATH = {
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath:
-        process.env.NODE_ENV === 'dev' ? ENV_PATH.dev : ENV_PATH.test,
-      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+        process.env.NODE_ENV === ENV_VAR.dev ? ENV_PATH.dev : ENV_PATH.test,
+      ignoreEnvFile: process.env.NODE_ENV === ENV_VAR.prod,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid(ENV_VAR.dev, ENV_VAR.test, ENV_VAR.prod)
+          .required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_NAME),
+      port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
