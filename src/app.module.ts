0e7c560c4a31f3db11a2dc0fca44fa1719149f99
허정호 <heojeongho1992@gmail.com>
2021-11-17 20:19:51 +0900

    add - ConfigModule for handling env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+
+const ENV_PATH = {
+  dev: '.env.dev',
+  test: '.env.test',
+};
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'dev' ? ENV_PATH.dev : ENV_PATH.test,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
