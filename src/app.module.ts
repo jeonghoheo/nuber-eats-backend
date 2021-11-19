7d0c3612fddc338d118e07d9345b6760069f2bb1
허정호 <heojeongho1992@gmail.com>
2021-11-19 21:29:06 +0900

    mod - 프로덕트 모드가 아닐시 log를 남기지 않기

--- src/app.module.ts
+++ src/app.module.ts
@@ -42,7 +42,7 @@ const ENV_PATH = {
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
-      logging: true,
+      logging: process.env.NODE_ENV !== 'prod',
       entities: [Restaurant],
     }),
     GraphQLModule.forRoot({
