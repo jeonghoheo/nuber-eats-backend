b74e94fb800151beb82a6da0243f169b601a66c7
허정호 <heojeongho1992@gmail.com>
2021-11-19 21:16:12 +0900

    add - TypeOrmModule을 주입시켜 사용할 repository를 가져와서 사용
    - imports에 사용하고싶은 모듈을 넣으면 해당 모듈에서 제공하는 메서드에 접근할수 있다.

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,15 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
+import RestaurantService from './restaurants.service';
 
 @Resolver()
 export class RestaurantsResolver {
+  constructor(private readonly service: RestaurantService) {}
+
   @Query((returns) => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.service.getAll();
   }
 
   @Mutation((returns) => Boolean)
