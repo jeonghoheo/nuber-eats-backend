ce1b145a717f84d85d0e602ccdc2bcbd29983953
허정호 <heojeongho1992@gmail.com>
2021-11-19 20:34:36 +0900

    add - Typeorm으로 테이블 생성하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
+import { Restaurant } from './restaurants/entities/restaurant.entity';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 const ENV_VAR = {
@@ -40,8 +41,9 @@ const ENV_PATH = {
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      synchronize: true,
+      synchronize: process.env.NODE_ENV !== 'prod',
       logging: true,
+      entities: [Restaurant],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,26 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field((type) => Number)
+  id: number;
+
+  @Column()
   @Field((type) => String)
   readonly name: string;
 
+  @Column()
   @Field((type) => Boolean, { nullable: true })
   readonly isVegan?: boolean;
 
+  @Column()
   @Field((type) => String)
   readonly address: string;
 
+  @Column()
   @Field((type) => String)
   readonly ownerName: string;
 }
